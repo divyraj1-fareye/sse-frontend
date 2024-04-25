dbd0526dbe5c1795012bfc0eec714616fb4b66e4
divyraj1-fareye <divyraj1@fareye.com>
2024-04-25 14:39:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  const [dataArray, setDataArray] = useState([]);
+
+
+  useEffect(()=>{
+   
+    const eventSource = new EventSource("http://localhost:8081/server-events/events");
+
+      eventSource.onmessage = (event) => {
+       let data = JSON.parse(event.data)
+       setDataArray(prevArray => [data, ...prevArray]);
+     };
+
+    return () => {
+      eventSource.close();
+    };
+
+  },[])
+
+
+  return  (
+
+    <div style={{backgroundColor:'pink'}}>
+      
+        {dataArray.map((item, index) => (
+          <div  style={{height:100,width:500,backgroundColor:'yellow',margin:'50px', border:'2px solid red', borderRadius:'5px'}}  key={index}>
+ 
+            <div  style={{ height:30,display:'flex',justifyContent:"center" ,paddingTop:30}} >
+             <span>Name :</span>
+             <span>{item.name}</span>
+            </div>
+           <div  style={{ height:30,display:'flex',justifyContent:"center" }} >
+             <span>Entry Time :</span>
+             <span>{item.localDateTime}</span>
+           </div> 
+          </div>
+       ))}
     </div>
-  );
+
+  )
+
 }
 
 export default App;
